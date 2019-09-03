916d657e60ca95d5b75dd52211b587a7b88392d3
Aleksandar Barisic <abarisic@hotmail.com>
2019-09-03 15:26:27 +0200

    Metro UI Design

---
+++ Lifeware_Metro_UI/Login.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Lifeware_Metro_UI
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Login = new Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLabel();
+            this.lbl_Password = new Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLabel();
+            this.lbl_regUsername = new Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLabel();
+            this.lbl_regPassword = new Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLabel();
+            this.lbl_regConfirm = new Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLabel();
+            this.tbx_Login = new Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLineTextField();
+            this.tbx_Passwd = new Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLineTextField();
+            this.tbx_reg_login = new Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLineTextField();
+            this.tbx_reg_passwd = new Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLineTextField();
+            this.tbx_reg_confirm_passwd = new Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLineTextField();
+            this.btn_registrieren = new Zeroit.Framework.Button.ZeroitDummyControl();
+            this.btn_anmelden = new Zeroit.Framework.Button.ZeroitDummyControl();
+            this.SuspendLayout();
+            // 
+            // lbl_Login
+            // 
+            this.lbl_Login.AllowTransparency = true;
+            this.lbl_Login.Depth = 0;
+            this.lbl_Login.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lbl_Login.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbl_Login.Location = new System.Drawing.Point(82, 117);
+            this.lbl_Login.MouseState = Zeroit.Framework.MaterialDesign.MouseState.HOVER;
+            this.lbl_Login.Name = "lbl_Login";
+            this.lbl_Login.Size = new System.Drawing.Size(122, 19);
+            this.lbl_Login.TabIndex = 0;
+            this.lbl_Login.Text = "Username/E-Mail";
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AllowTransparency = true;
+            this.lbl_Password.Depth = 0;
+            this.lbl_Password.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lbl_Password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbl_Password.Location = new System.Drawing.Point(82, 157);
+            this.lbl_Password.MouseState = Zeroit.Framework.MaterialDesign.MouseState.HOVER;
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(71, 19);
+            this.lbl_Password.TabIndex = 1;
+            this.lbl_Password.Text = "Password";
+            // 
+            // lbl_regUsername
+            // 
+            this.lbl_regUsername.AllowTransparency = true;
+            this.lbl_regUsername.Depth = 0;
+            this.lbl_regUsername.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lbl_regUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbl_regUsername.Location = new System.Drawing.Point(82, 324);
+            this.lbl_regUsername.MouseState = Zeroit.Framework.MaterialDesign.MouseState.HOVER;
+            this.lbl_regUsername.Name = "lbl_regUsername";
+            this.lbl_regUsername.Size = new System.Drawing.Size(122, 19);
+            this.lbl_regUsername.TabIndex = 2;
+            this.lbl_regUsername.Text = "Username/E-Mail";
+            // 
+            // lbl_regPassword
+            // 
+            this.lbl_regPassword.AllowTransparency = true;
+            this.lbl_regPassword.Depth = 0;
+            this.lbl_regPassword.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lbl_regPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbl_regPassword.Location = new System.Drawing.Point(82, 365);
+            this.lbl_regPassword.MouseState = Zeroit.Framework.MaterialDesign.MouseState.HOVER;
+            this.lbl_regPassword.Name = "lbl_regPassword";
+            this.lbl_regPassword.Size = new System.Drawing.Size(71, 19);
+            this.lbl_regPassword.TabIndex = 3;
+            this.lbl_regPassword.Text = "Password";
+            // 
+            // lbl_regConfirm
+            // 
+            this.lbl_regConfirm.AllowTransparency = true;
+            this.lbl_regConfirm.Depth = 0;
+            this.lbl_regConfirm.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lbl_regConfirm.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbl_regConfirm.Location = new System.Drawing.Point(82, 402);
+            this.lbl_regConfirm.MouseState = Zeroit.Framework.MaterialDesign.MouseState.HOVER;
+            this.lbl_regConfirm.Name = "lbl_regConfirm";
+            this.lbl_regConfirm.Size = new System.Drawing.Size(59, 19);
+            this.lbl_regConfirm.TabIndex = 4;
+            this.lbl_regConfirm.Text = "Confirm";
+            // 
+            // tbx_Login
+            // 
+            this.tbx_Login.Depth = 0;
+            this.tbx_Login.Hint = "";
+            this.tbx_Login.Location = new System.Drawing.Point(258, 112);
+            this.tbx_Login.MaxLength = 32767;
+            this.tbx_Login.MouseState = Zeroit.Framework.MaterialDesign.MouseState.HOVER;
+            this.tbx_Login.Name = "tbx_Login";
+            this.tbx_Login.PasswordChar = '\0';
+            this.tbx_Login.SelectedText = "";
+            this.tbx_Login.SelectionLength = 0;
+            this.tbx_Login.SelectionStart = 0;
+            this.tbx_Login.Size = new System.Drawing.Size(292, 23);
+            this.tbx_Login.TabIndex = 5;
+            this.tbx_Login.TabStop = false;
+            this.tbx_Login.UseSystemPasswordChar = false;
+            // 
+            // tbx_Passwd
+            // 
+            this.tbx_Passwd.Depth = 0;
+            this.tbx_Passwd.Hint = "";
+            this.tbx_Passwd.Location = new System.Drawing.Point(258, 153);
+            this.tbx_Passwd.MaxLength = 32767;
+            this.tbx_Passwd.MouseState = Zeroit.Framework.MaterialDesign.MouseState.HOVER;
+            this.tbx_Passwd.Name = "tbx_Passwd";
+            this.tbx_Passwd.PasswordChar = '\0';
+            this.tbx_Passwd.SelectedText = "";
+            this.tbx_Passwd.SelectionLength = 0;
+            this.tbx_Passwd.SelectionStart = 0;
+            this.tbx_Passwd.Size = new System.Drawing.Size(292, 23);
+            this.tbx_Passwd.TabIndex = 6;
+            this.tbx_Passwd.TabStop = false;
+            this.tbx_Passwd.UseSystemPasswordChar = false;
+            // 
+            // tbx_reg_login
+            // 
+            this.tbx_reg_login.Depth = 0;
+            this.tbx_reg_login.Hint = "";
+            this.tbx_reg_login.Location = new System.Drawing.Point(258, 320);
+            this.tbx_reg_login.MaxLength = 32767;
+            this.tbx_reg_login.MouseState = Zeroit.Framework.MaterialDesign.MouseState.HOVER;
+            this.tbx_reg_login.Name = "tbx_reg_login";
+            this.tbx_reg_login.PasswordChar = '\0';
+            this.tbx_reg_login.SelectedText = "";
+            this.tbx_reg_login.SelectionLength = 0;
+            this.tbx_reg_login.SelectionStart = 0;
+            this.tbx_reg_login.Size = new System.Drawing.Size(292, 23);
+            this.tbx_reg_login.TabIndex = 7;
+            this.tbx_reg_login.TabStop = false;
+            this.tbx_reg_login.UseSystemPasswordChar = false;
+            // 
+            // tbx_reg_passwd
+            // 
+            this.tbx_reg_passwd.Depth = 0;
+            this.tbx_reg_passwd.Hint = "";
+            this.tbx_reg_passwd.Location = new System.Drawing.Point(258, 361);
+            this.tbx_reg_passwd.MaxLength = 32767;
+            this.tbx_reg_passwd.MouseState = Zeroit.Framework.MaterialDesign.MouseState.HOVER;
+            this.tbx_reg_passwd.Name = "tbx_reg_passwd";
+            this.tbx_reg_passwd.PasswordChar = '\0';
+            this.tbx_reg_passwd.SelectedText = "";
+            this.tbx_reg_passwd.SelectionLength = 0;
+            this.tbx_reg_passwd.SelectionStart = 0;
+            this.tbx_reg_passwd.Size = new System.Drawing.Size(292, 23);
+            this.tbx_reg_passwd.TabIndex = 8;
+            this.tbx_reg_passwd.TabStop = false;
+            this.tbx_reg_passwd.UseSystemPasswordChar = false;
+            // 
+            // tbx_reg_confirm_passwd
+            // 
+            this.tbx_reg_confirm_passwd.Depth = 0;
+            this.tbx_reg_confirm_passwd.Hint = "";
+            this.tbx_reg_confirm_passwd.Location = new System.Drawing.Point(258, 398);
+            this.tbx_reg_confirm_passwd.MaxLength = 32767;
+            this.tbx_reg_confirm_passwd.MouseState = Zeroit.Framework.MaterialDesign.MouseState.HOVER;
+            this.tbx_reg_confirm_passwd.Name = "tbx_reg_confirm_passwd";
+            this.tbx_reg_confirm_passwd.PasswordChar = '\0';
+            this.tbx_reg_confirm_passwd.SelectedText = "";
+            this.tbx_reg_confirm_passwd.SelectionLength = 0;
+            this.tbx_reg_confirm_passwd.SelectionStart = 0;
+            this.tbx_reg_confirm_passwd.Size = new System.Drawing.Size(292, 23);
+            this.tbx_reg_confirm_passwd.TabIndex = 9;
+            this.tbx_reg_confirm_passwd.TabStop = false;
+            this.tbx_reg_confirm_passwd.UseSystemPasswordChar = false;
+            // 
+            // btn_registrieren
+            // 
+            this.btn_registrieren.AllowClickAnimation = true;
+            this.btn_registrieren.AllowTransparency = true;
+            this.btn_registrieren.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.btn_registrieren.BorderWidth = 2;
+            this.btn_registrieren.ClickMaxOffset = 10;
+            this.btn_registrieren.ClickOffset = 1;
+            this.btn_registrieren.ClickSpeed = 1;
+            this.btn_registrieren.ColorShape = false;
+            this.btn_registrieren.Curve = 10;
+            this.btn_registrieren.DownLeftCurve = 10;
+            this.btn_registrieren.DownRightCurve = 10;
+            this.btn_registrieren.DrawBorder = true;
+            this.btn_registrieren.EndAngle = 90F;
+            this.btn_registrieren.Image = null;
+            this.btn_registrieren.ImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_registrieren.ImageSize = new System.Drawing.Size(0, 0);
+            this.btn_registrieren.Location = new System.Drawing.Point(258, 443);
+            this.btn_registrieren.Name = "btn_registrieren";
+            this.btn_registrieren.PolygonRadius = 10;
+            this.btn_registrieren.PolygonSides = 3;
+            this.btn_registrieren.PolygonStartingAngle = 90;
+            this.btn_registrieren.Rounding = true;
+            this.btn_registrieren.Shape = Zeroit.Framework.Button.Shapes.Rectangle;
+            this.btn_registrieren.ShapeColor = System.Drawing.Color.Yellow;
+            this.btn_registrieren.ShapePopulate = new Zeroit.Framework.Button.ShapePopulate(Zeroit.Framework.Button.Shapes.Rectangle, System.Drawing.Color.DeepSkyBlue, System.Drawing.Color.Yellow, true, false, true, 10, 10, 10, 10, 10);
+            this.btn_registrieren.ShowText = true;
+            this.btn_registrieren.Size = new System.Drawing.Size(292, 23);
+            this.btn_registrieren.StartAngle = 0F;
+            this.btn_registrieren.TabIndex = 10;
+            this.btn_registrieren.Text = "Registieren";
+            this.btn_registrieren.TextAlign = System.Drawing.StringAlignment.Center;
+            this.btn_registrieren.UpperLeftCurve = 10;
+            this.btn_registrieren.UpperRightCurve = 10;
+            // 
+            // btn_anmelden
+            // 
+            this.btn_anmelden.AllowClickAnimation = true;
+            this.btn_anmelden.AllowTransparency = true;
+            this.btn_anmelden.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.btn_anmelden.BorderWidth = 2;
+            this.btn_anmelden.ClickMaxOffset = 10;
+            this.btn_anmelden.ClickOffset = 1;
+            this.btn_anmelden.ClickSpeed = 1;
+            this.btn_anmelden.ColorShape = false;
+            this.btn_anmelden.Curve = 10;
+            this.btn_anmelden.DownLeftCurve = 10;
+            this.btn_anmelden.DownRightCurve = 10;
+            this.btn_anmelden.DrawBorder = true;
+            this.btn_anmelden.EndAngle = 90F;
+            this.btn_anmelden.Image = null;
+            this.btn_anmelden.ImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_anmelden.ImageSize = new System.Drawing.Size(0, 0);
+            this.btn_anmelden.Location = new System.Drawing.Point(258, 196);
+            this.btn_anmelden.Name = "btn_anmelden";
+            this.btn_anmelden.PolygonRadius = 10;
+            this.btn_anmelden.PolygonSides = 3;
+            this.btn_anmelden.PolygonStartingAngle = 90;
+            this.btn_anmelden.Rounding = true;
+            this.btn_anmelden.Shape = Zeroit.Framework.Button.Shapes.Rectangle;
+            this.btn_anmelden.ShapeColor = System.Drawing.Color.Yellow;
+            this.btn_anmelden.ShapePopulate = new Zeroit.Framework.Button.ShapePopulate(Zeroit.Framework.Button.Shapes.Rectangle, System.Drawing.Color.DeepSkyBlue, System.Drawing.Color.Yellow, true, false, true, 10, 10, 10, 10, 10);
+            this.btn_anmelden.ShowText = true;
+            this.btn_anmelden.Size = new System.Drawing.Size(292, 23);
+            this.btn_anmelden.StartAngle = 0F;
+            this.btn_anmelden.TabIndex = 11;
+            this.btn_anmelden.Text = "Anmelden";
+            this.btn_anmelden.TextAlign = System.Drawing.StringAlignment.Center;
+            this.btn_anmelden.UpperLeftCurve = 10;
+            this.btn_anmelden.UpperRightCurve = 10;
+            this.btn_anmelden.Click += new System.EventHandler(this.Btn_anmelden_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(774, 513);
+            this.Controls.Add(this.btn_anmelden);
+            this.Controls.Add(this.btn_registrieren);
+            this.Controls.Add(this.tbx_reg_confirm_passwd);
+            this.Controls.Add(this.tbx_reg_passwd);
+            this.Controls.Add(this.tbx_reg_login);
+            this.Controls.Add(this.tbx_Passwd);
+            this.Controls.Add(this.tbx_Login);
+            this.Controls.Add(this.lbl_regConfirm);
+            this.Controls.Add(this.lbl_regPassword);
+            this.Controls.Add(this.lbl_regUsername);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_Login);
+            this.MinimumSize = new System.Drawing.Size(126, 39);
+            this.Name = "Login";
+            this.Text = "Anmeldung";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLabel lbl_Login;
+        private Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLabel lbl_Password;
+        private Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLabel lbl_regUsername;
+        private Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLabel lbl_regPassword;
+        private Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLabel lbl_regConfirm;
+        private Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLineTextField tbx_Login;
+        private Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLineTextField tbx_Passwd;
+        private Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLineTextField tbx_reg_login;
+        private Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLineTextField tbx_reg_passwd;
+        private Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialLineTextField tbx_reg_confirm_passwd;
+        private Zeroit.Framework.Button.ZeroitDummyControl btn_registrieren;
+        private Zeroit.Framework.Button.ZeroitDummyControl btn_anmelden;
+    }
+}
+
---
+++ Lifeware_Metro_UI/Login.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+using System.Security.Cryptography;
+
+
+namespace Lifeware_Metro_UI
+{
+    public partial class Login : Zeroit.Framework.MaterialDesign.Controls.ZeroitMaterialForm
+    {
+
+        DatabaseConnectingHandler db = new DatabaseConnectingHandler();
+        Logins login = new Logins();
+
+        public int userID;
+
+
+        public Login()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Btn_anmelden_Click(object sender, EventArgs e)
+        {
+            string username = tbx_Login.Text;
+            string password = HPass(tbx_Passwd.Text);
+
+
+            foreach (var user in db.Logins)
+            {
+
+                if (user.Username == username && user.Password == password)
+                {
+                    tbx_Login.Text = string.Empty;
+                    tbx_Passwd.Text = string.Empty;
+
+                    userID = user.Id_Login;
+
+                    MessageBox.Show(userID.ToString());
+
+                }
+            }
+        }
+
+        private void AddUser(string username, string password, string confirmPass)
+        {
+            foreach (var user in db.Logins)
+            {
+                if (user.Username.Equals(username))
+                {
+                    MessageBox.Show("Dieser Nutzer exsistiert !");
+                    return;
+
+                }
+            }
+
+            if (password != confirmPass)
+            {
+                MessageBox.Show("Password stimmt nicht überein!");
+            }
+
+            else if (password.Length < 8)
+            {
+                MessageBox.Show("Das Passwort muss mindestens 8 Zeichen haben.");
+            }
+            else
+            {
+                string Epass = HPass(password);
+                login.Username = username;
+
+            }
+        }
+
+        public string HPass(string password)
+        {
+            SHA512 sha = new SHA512CryptoServiceProvider();
+            sha.ComputeHash(ASCIIEncoding.ASCII.GetBytes(password));
+            byte[] result = sha.Hash;
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+
+            return strBuilder.ToString();
+        }
+    }
+}
